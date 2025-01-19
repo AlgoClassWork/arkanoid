2f95c32c725d68b636c31456d648bcdbe0b4a29c
AlgoClassWork <kyxec.play@gmail.com>
2025-01-19 11:51:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pygame import *
 
-window = display.set_mode((700,500))
+window = display.set_mode((500,500))
 display.set_caption('Арканойд')
 
 class GameSprite(sprite.Sprite):
@@ -17,20 +17,22 @@ class GameSprite(sprite.Sprite):
 platform = GameSprite('platform.png', 350, 450)
 
 ball = GameSprite('ball.png', 300, 300)
-speed_x, speed_y = 1, 1
+speed_x, speed_y = 5, 5
 
 monsters = sprite.Group()
-start_x, start_y, count = 20, 5, 9
+start_x, start_y, count = 5, 5, 9
 for i in range(3): 
     y = start_y + (55 * i) 
-    x = start_x + (35 * i) 
+    x = start_x + (28 * i) 
 
     for i in range(count):
         monster = GameSprite('enemy.png', x, y)
         monsters.add(monster)
-        x += 75
+        x += 55
 
     count -= 1
+
+clock = time.Clock()
     
 game = True
 while game:
@@ -39,8 +41,10 @@ while game:
             game = False
 
     window.fill((155,255,255))
+
     platform.reset()
     ball.reset()
+
     monsters.draw(window)
 
     mouse_x, mouse_y = mouse.get_pos()
@@ -49,11 +53,14 @@ while game:
     ball.rect.x += speed_x 
     ball.rect.y += speed_y
 
-    if ball.rect.x > 650 or ball.rect.x < 0:
+    if ball.rect.x > 450 or ball.rect.x < 0:
         speed_x *= -1
 
     if sprite.collide_rect(platform,ball) or ball.rect.y < 0:
         speed_y *= -1
 
+    if sprite.spritecollide(ball, monsters, True):
+        speed_y *= -1
 
     display.update()
+    clock.tick(60)
